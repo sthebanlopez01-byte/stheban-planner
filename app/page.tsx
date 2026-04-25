179c4cfacf485e9ba8d912c3e961ebdd91204e0f
sthebanlopez01-byte <sthebanlopez01@gmail.com>
2026-04-25 16:35:34 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Planificador Stheban</h1>
+      <p>Tu app ya está en línea 🚀</p>
+    </div>
+  );
+}
